4c84cc8ca99b07e08ea80126ec51cea3a97fb7ca
Sincere <sincerityegbu@gmail.com>
2024-02-14 14:21:52 +0000

    Seperated inputs from the app file into a seperate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { useRef } from "react";
-import { v4 as uuid} from "uuid";
 import { Query } from "./Query";
+import { QueryInput } from "./QueryInput";
 
 export const App = () => {
-	const inputRef = useRef<HTMLInputElement>(null);
-
-	const handleSubmit = () => {
-		if (inputRef.current){
-			let value = inputRef.current.value;
-
-			if (value != "")
-				sessionStorage.setItem(uuid(), value);
-
-			inputRef.current.value = "";
-		}
-		else 
-			alert("error");
-	}
-
-	const handleClear = () => {
-		sessionStorage.clear();
-	}
-
     return ( 
 		<div>
 			{Object.keys(sessionStorage).map((id) => {
@@ -29,12 +9,9 @@ export const App = () => {
 					<Query key={id} id={id} content={sessionStorage.getItem(id) as string}/>
 				)
 			})}
+			
 
-			<form onSubmit={handleSubmit}>
-				<input type="text" ref={inputRef}/>
-				<button onClick={handleClear}>Clear</button>
-				<input type="submit"/>
-			</form>
+			<QueryInput/>
 		</div>
 	);
 }
---
+++ src/QueryInput.tsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import { v4 as uuid} from "uuid";
+
+export const QueryInput = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+	const handleSubmit = () => {
+		if (inputRef.current){
+			let value = inputRef.current.value;
+
+			if (value != "")
+				sessionStorage.setItem(uuid(), value);
+
+			inputRef.current.value = "";
+		}
+		else 
+			alert("error");
+	}
+
+	const handleClear = () => {
+		sessionStorage.clear();
+	}
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" ref={inputRef}/>
+            <button onClick={handleClear}>Clear</button>
+            <input type="submit"/>
+        </form>
+    )
+}
