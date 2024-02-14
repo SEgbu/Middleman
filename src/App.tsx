4e1271adb66c9ccc6c169c3566a57c613e282302
Sincere <sincerityegbu@gmail.com>
2024-02-14 13:53:45 +0000

    added posting queries to session storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-function App() {
+import { useEffect, useRef } from "react";
+import { v4 as uuid} from "uuid";;
+
+export const App = () => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	const handleSubmit = () => {
+		if (inputRef.current){
+			let value = inputRef.current.value;
+
+			if (value != "")
+				sessionStorage.setItem(uuid(), JSON.stringify({queryData: value}));
+
+			inputRef.current.value = "";
+		}
+		else 
+			alert("error");
+	}
+
+	const handleClear = () => {
+		sessionStorage.clear();
+	}
+
     return ( 
 		<>
-			<p>Middleman</p>
+			<form onSubmit={handleSubmit}>
+				<input type="text" ref={inputRef}/>
+				<button onClick={handleClear}>Clear</button>
+				<input type="submit"/>
+			</form>
+				
+			{Object.keys(sessionStorage).map((x) => {
+				return <p key={x}>{sessionStorage.getItem(x)}</p>
+			})}
 		</>
 	);
 }
