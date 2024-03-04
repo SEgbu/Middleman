47aebb5d112aa4cc35caf7c1637e42df946f67df
Sincere <sincerityegbu@gmail.com>
2024-03-04 22:37:48 +0000

    Store submission files in storage and added submission counter

--- src/Home/Query.tsx
+++ src/Home/Query.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import supabase from "../../config/Supabase";
 
@@ -8,7 +8,7 @@ type QueryType = {
 };
 
 type submissionDataType = {
-    filePath: string; 
+    filePath: string;
     queryId: number;
 };
 
@@ -19,15 +19,37 @@ export const Query = (props: QueryType) => {
 
     const [queryRemoveError, setQueryRemoveError] = useState<string>("");
 
+    const [submissionsCount, setSubmissionCount] = useState<number>(0);
+
     const submissionRef = useRef<HTMLInputElement>(null);
 
-    const handleRemove = async () => {
-        const {error} = await supabase
-                                .from("queries")
-                                .delete()
-                                .eq("id", data.id);
+    useEffect(() => {
+        const fetchMySubmissionFile = async () => {
+            if (submissionRef.current && submissionRef.current.files) {
+                const { data: submissionInfo, error: submissionInfoError } = await supabase
+                    .from("submissions")
+                    .select()
+                    .eq("queryId", data.id);
 
-        if (error){
+                if (submissionInfoError) {
+                    console.log("Retrieve Submission Info Error: " + submissionInfoError?.message);
+                }
+                else {
+                    setSubmissionCount(submissionInfo.length)
+                }
+
+            }
+        }
+        fetchMySubmissionFile();
+    }, [])
+
+    const handleRemove = async () => {
+        const { error } = await supabase
+            .from("queries")
+            .delete()
+            .eq("id", data.id);
+
+        if (error) {
             setQueryRemoveError("Can't remove query");
             console.log(queryRemoveError + ": " + error.message);
         }
@@ -37,19 +59,32 @@ export const Query = (props: QueryType) => {
     }
 
     const handleSubmission = async () => {
-
-        if (submissionRef.current && submissionRef.current.files){
-            let submissionData : submissionDataType = {
-                filePath: "public/"+submissionRef.current.files[0].name,
+        // insert submission info and upload submission file to storage
+        if (submissionRef.current && submissionRef.current.files) {
+            let submissionData: submissionDataType = {
+                filePath: "public/" + submissionRef.current.files[0].name,
                 queryId: data.id
             }
 
-            const {error} = await supabase
-                                        .from("submissions")
-                                        .insert({filePath: submissionData.filePath, queryId: submissionData.queryId});
+            const { error: uploadError } = await supabase
+                .storage
+                .from("submissions")
+                .upload(submissionData.filePath, submissionRef.current.files[0], { cacheControl: "3600", upsert: false });
 
-            if (error){
-                console.log("Submission Error: " + error.message);
+            if (uploadError) {
+                console.log("Submission Upload Error: " + uploadError.message)
+            }
+            else {
+                const { error: insertError } = await supabase
+                .from("submissions")
+                .insert({ filePath: submissionData.filePath, queryId: submissionData.queryId });
+
+                if (insertError) {
+                    console.log("Submission Insert Error: " + insertError.message);
+                }
+                else {
+                    setSubmissionCount(submissionsCount + 1)
+                }
             }
         }
     }
@@ -59,7 +94,7 @@ export const Query = (props: QueryType) => {
             <label htmlFor="info">{data.query} </label>
 
             {
-                data.description != "" &&  
+                data.description != "" &&
                 <>
                     <button id="info" onClick={() => {
                         setInfoShown(!isInfoShown);
@@ -74,6 +109,8 @@ export const Query = (props: QueryType) => {
             }
             <button onClick={handleRemove}>Remove</button>
             <Link to="/edit" state={data}><button>Edit</button></Link>
-            <input type="file" ref={submissionRef} onChange={handleSubmission}/>        </div>
+            <span> Submission Counter: {submissionsCount} </span>
+            <input type="file" id="submission" ref={submissionRef} onChange={handleSubmission} />
+        </div>
     )
 }
